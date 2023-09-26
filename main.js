a4f3897aadfe0f4eb76d4f7d2c6db495296671bf
Deadabdul <a.oguntade11@gmail.com>
2023-09-26 13:42:51 -0600

    finished

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 // HTML Variables
 let outputEl = document.getElementById("output");
+let rankingList = [];
 
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
+
+if(localStorage.getItem("list") != null) {
+   rankingList = localStorage.getItem("list").split(",");
+  printList();
+}
+else console.log("No List");
 
 function btnClicked() {
   // Get Menu Selection
@@ -28,25 +35,97 @@ function btnClicked() {
 
 // Menu Option Functions
 function addItem() {
-  console.log("Add Item");
+  // Prompt the user for an item to add to the ranking list
+  let newItem = prompt("Enter the item to add:");
+  if (newItem !== null) {
+    // Add the item to the end of the list
+    rankingList.push(newItem);
+    printList();
+  }
 }
 
 function removeLast() {
-  console.log("Remove Last");
+  if (rankingList.length <= 0) {alert("There are no items in the ranking list."); return;}
+
+  // Remove the last item from the ranking list
+  if (rankingList.length > 0) {
+    rankingList.pop();
+    printList();
+  }
 }
 
 function insert() {
-  console.log("Insert");
+  if (rankingList.length <= 0) {alert("There are no items in the ranking list."); return;}
+
+  // Prompt the user for a position and an item to insert
+  let position = +prompt("Enter the position to insert (1 - " + (rankingList.length + 1) + "):");
+  if ( position >= 1 && position <= rankingList.length + 1) {
+    let newItem = prompt("Enter the item to insert:");
+    if (newItem !== null) {
+      // Insert the provided item at the specified position
+      rankingList.splice(position - 1, 0, newItem);
+      printList();
+    }
+  } else {
+    alert("Invalid position. Please enter a valid position.");
+  }
 }
 
 function removePos() {
-  console.log("Remove at Position");
+  if (rankingList.length <= 0) {alert("There are no items in the ranking list."); return;}
+
+  // Prompt the user for a position
+  let position = +prompt("Enter the position to remove (1 - " + rankingList.length + "):");
+  if (position >= 1 && position <= rankingList.length) {
+    // Remove the item at the given position
+    rankingList.splice(position - 1, 1);
+    printList();
+  } else {
+    alert("Invalid position. Please enter a valid position.");
+  }
 }
 
 function move() {
-  console.log("Move");
+  if (rankingList.length <= 0) {alert("There are no items in the ranking list."); return;}
+
+  // Prompt the user for positions to move from and to
+  let fromPosition = +prompt("Enter the position to move from (1 - " + rankingList.length + "):");
+  let toPosition = +prompt("Enter the position to move to (1 - " + rankingList.length + "):");
+
+  if (fromPosition >= 1 && fromPosition <= rankingList.length &&
+      toPosition >= 1 && toPosition <= rankingList.length) {
+    // Move the item according to the user’s instructions
+    let itemToMove = rankingList.splice(fromPosition - 1, 1)[0];
+    rankingList.splice(toPosition - 1, 0, itemToMove);
+    printList();
+  } else {
+    alert("Invalid positions. Please enter valid positions.");
+  }
 }
 
 function edit() {
-  console.log("Edit");
+  // Prompt the user for a position and an item to replace
+  if (rankingList.length <= 0) {midalert("There are no items in the ranking list."); return;}
+  let position = +prompt("Enter the position to edit (1 - " + rankingList.length + "):");
+  if (position >= 1 && position <= rankingList.length) {
+    let newItem = prompt("Enter the new item to replace the value:");
+    if (newItem !== null) {
+      // Replace the item in the list according to the user’s instructions
+      rankingList[position - 1] = newItem;
+      printList();
+    }
+  } else {
+    alert("Invalid position. Please enter a valid position.");
+  }
+}
+
+// Function to print the updated ranking list
+function printList() {
+  outputEl.innerHTML = ""; // Clear the existing list
+  localStorage.setItem("list", rankingList);
+  for (let i = 0; i < rankingList.length; i++) {
+    let listItem = document.createElement("p");
+    listItem.textContent = (i + 1) + ". " + rankingList[i];
+    outputEl.appendChild(listItem);
+  }
 }
